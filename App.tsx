3ddda24c32ea3f60a9aada4cc991b49d2277736f
Mohamed Baher <mohamed.baher@valify.me>
2024-09-29 12:20:56 +0300

    -first commit

--- App.tsx
+++ App.tsx
@@ -1,117 +1,233 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Button,
+  Alert,
+  ActivityIndicator,
 } from 'react-native';
+import { startOCR } from '@valifysolutions/react-native-vidvocr';
+import { startLiveness } from '@valifysolutions/react-native-vidvliveness';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const testOCR = true;
+const testLiveness = true;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// Unified credentials
+const creds = {
+  baseURL: 'https://www.valifystage.com/', // Update with your actual base URL
+  bundleKey: 'ad44eb94ca6747beaf99eef02407221f', // Replace with your actual bundle key
+  userName: 'mobileusername', // Replace with actual credentials
+  password: 'q5YT54wuJ2#mbanR',
+  clientID: 'aKM21T4hXpgHFsgNJNTKFpaq4fFpoQvuBsNWuZoQ',
+  clientSecret: 'r0tLrtxTue8c4kNmPVgaAFNGSeCWvL4oOZfBnVXoQe2Ffp5rscXXAAhX50BaZEll8ZRtr2BlgD3Nk6QLOPGtjbGXYoCBL9Fn7QCu5CsMlRKDbtwSnUAfKEG30cIv8tdW',
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const getToken = async () => {
+  const url = `${creds.baseURL}/api/o/token/`;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const body = `username=${creds.userName}&password=${creds.password}&client_id=${creds.clientID}&client_secret=${creds.clientSecret}&grant_type=password`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: body,
+    });
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse.access_token;
+    } else {
+      throw new Error('Failed to retrieve token');
+    }
+  } catch (error) {
+    console.error(error);
+    Alert.alert('Error', 'Could not generate token');
+    return null;
+  }
+};
+
+const App = (): JSX.Element => {
+  const [loading, setLoading] = useState(false);
+
+  const [loadingOCR, setLoadingOCR] = useState(false);
+  const [loadingLiveness, setLoadingLiveness] = useState(false);
+
+  const handleStartValify = async () => {
+    setLoading(true);
+
+    const token = await getToken();
+
+    if (token) {
+      // Start OCR Process
+      const ocrParams = {
+        access_token: token,
+        base_url: creds.baseURL,
+        bundle_key: creds.bundleKey,
+        language: 'en',
+        review_data: true
+      };
+
+      startOCR(ocrParams)
+        .then((ocrResponse) => {
+          console.log('OCR Result:', ocrResponse);
+
+          // Parse the OCR response if it’s a string
+          const parsedResponse = typeof ocrResponse === 'string' ? JSON.parse(ocrResponse) : ocrResponse;
+
+          // Check the OCR result state
+          if (parsedResponse.nameValuePairs?.state === "SUCCESS") {
+            const transactionIdFront = parsedResponse.nameValuePairs?.ocrResult?.ocrResult?.transactionIdFront;
+            console.log('Transaction ID Front:', transactionIdFront);
+            if(testLiveness){
+                           if (transactionIdFront) {
+
+                              // Wait 2 seconds before starting liveness experience
+                              setTimeout(() => {
+                                const livenessParams = {
+                                  access_token: token,
+                                  base_url: creds.baseURL,
+                                  bundle_key: creds.bundleKey,
+                                  language: 'en',
+                                  facematch_ocr_transactionId: transactionIdFront, // Pass the transaction ID from OCR
+                                };
+
+
+                                startLiveness(livenessParams)
+                                  .then((livenessValue) => {
+                                    console.log('Liveness Result:', livenessValue);
+                                    Alert.alert('Success', 'Liveness completed successfully');
+                                  })
+                                  .catch((livenessError) => {
+                                    console.error('Liveness Error:', livenessError);
+                                    Alert.alert('Error', 'Liveness failed');
+                                  });
+                              }, 2000); // 2 seconds delay
+                            } else {
+                              Alert.alert('Error', 'Transaction ID not found in OCR response');
+                            }
+                        }
+
+
+          } else {
+            console.log('Current OCR state is not SUCCESS');
+          }
+        })
+        .catch((ocrError) => {
+          console.error('OCR Error:', ocrError);
+          Alert.alert('Error', 'OCR failed');
+        })
+        .finally(() => setLoading(false));
+    }
   };
+    const handleStartLiveness = async () => {
+      setLoadingLiveness(true);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+          const token = await getToken();
+
+          if (token) {
+                      const livenessParams = {
+                        access_token: token,
+                        base_url: creds.baseURL,
+                        bundle_key: creds.bundleKey,
+                        language: 'en'
+                                        };
+
+                      startLiveness(livenessParams)
+                        .then((livenessValue) => {
+                          console.log('Liveness Result:', livenessValue);
+                          Alert.alert('Success', 'Liveness completed successfully');
+                        })
+                        .catch((livenessError) => {
+                          console.error('Liveness Error:', livenessError);
+                          Alert.alert('Error', 'Liveness failed');
+                        }).finally(() => setLoadingLiveness(false));
+          }else{
+                  Alert.alert('Error', 'Token failed');
+
+              }
+
+    };
+    const handleStartOCR = async () => {
+      setLoadingOCR(true);
+
+      const token = await getToken();
+
+      if (token) {
+        // Start OCR Process
+        const ocrParams = {
+          access_token: token,
+          base_url: creds.baseURL,
+          bundle_key: creds.bundleKey,
+          language: 'en',
+          review_data: true
+        };
+
+        startOCR(ocrParams)
+          .then((ocrResponse) => {
+            console.log('OCR Result:', ocrResponse);
+
+            // Parse the OCR response if it’s a string
+            const parsedResponse = typeof ocrResponse === 'string' ? JSON.parse(ocrResponse) : ocrResponse;
+
+            // Check the OCR result state
+            if (parsedResponse.nameValuePairs?.state === "SUCCESS") {
+              const transactionIdFront = parsedResponse.nameValuePairs?.ocrResult?.ocrResult?.transactionIdFront;
+              console.log('Transaction ID Front:', transactionIdFront);
+
+
+            } else {
+              console.log('Current OCR state is not SUCCESS');
+            }
+          })
+          .catch((ocrError) => {
+            console.error('OCR Error:', ocrError);
+            Alert.alert('Error', 'OCR failed');
+          })
+          .finally(() => setLoadingOCR(false));
+      }
+    };
+
+   return (
+     <SafeAreaView style={styles.container}>
+       <View style={styles.buttonContainer}>
+         {loading ? (
+           <ActivityIndicator size="large" color="#0000ff" />
+         ) : (
+           <Button title="Start Valify" onPress={handleStartValify} />
+         )}
+       </View>
+       <View style={styles.buttonContainer}>
+         {loadingOCR ? (
+           <ActivityIndicator size="large" color="#0000ff" />
+         ) : (
+           <Button title="Start OCR" onPress={handleStartOCR} />
+         )}
+       </View>
+       <View style={styles.buttonContainer}>
+         {loadingLiveness ? (
+           <ActivityIndicator size="large" color="#0000ff" />
+         ) : (
+           <Button title="Start Liveness" onPress={handleStartLiveness} />
+         )}
+       </View>
+     </SafeAreaView>
+   );
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center', // Center the buttons vertically
+    alignItems: 'center', // Center the buttons horizontally
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonContainer: {
+    marginBottom: 20, // Add space between buttons
+    width: '80%', // Optional: To set button width
   },
 });
 
